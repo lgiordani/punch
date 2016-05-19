4ad10f1a33ced8650f04dac760f1f1b4713ba394
PyTest <py.test@email.com>
2016-05-19 10:01:28 +0200

    Fixed missing change in VCSReleaseUseCase

--- punch/vcs_use_cases/release.py
+++ punch/vcs_use_cases/release.py
@@ -2,8 +2,8 @@ from punch.vcs_use_cases import use_case
 
 
 class VCSReleaseUseCase(use_case.VCSUseCase):
-    def run(self, release, commit_message):
-        self.pre_start_release(release)
-        self.start_release(release)
-        self.finish_release(release, commit_message)
-        self.post_finish_release(release)
+    def run(self):
+        self.pre_start_release()
+        self.start_release()
+        self.finish_release()
+        self.post_finish_release()
