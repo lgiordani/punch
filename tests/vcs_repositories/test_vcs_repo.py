355b1f96b407d3593f2c188c9600c24b326c721a
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-06-07 13:46:21 +0100

    Using mock instead of mocker to avoid context issues

--- tests/vcs_repositories/test_vcs_repo.py
+++ tests/vcs_repositories/test_vcs_repo.py
@@ -2,6 +2,11 @@ import pytest
 import six
 from punch.vcs_repositories import vcs_repo as vr, exceptions as re
 
+if six.PY2:
+    import mock
+else:
+    from unittest import mock
+
 pytestmark = pytest.mark.slow
 
 
@@ -10,8 +15,8 @@ def _test_set_command(self):
     self.command = 'ls'
 
 
-def test_init_without_program_installed(mocker, temp_empty_dir):
-    with mocker.patch("subprocess.Popen") as mock_popen:
+def test_init_without_program_installed(temp_empty_dir):
+    with mock.patch("subprocess.Popen") as mock_popen:
         if six.PY2:
             mock_popen.side_effect = IOError
         else:
@@ -19,16 +24,16 @@ def test_init_without_program_installed(mocker, temp_empty_dir):
 
         with pytest.raises(re.RepositorySystemError):
             vr.VCSRepo._set_command = _test_set_command
-            vr.VCSRepo(temp_empty_dir, mocker.Mock())
+            vr.VCSRepo(temp_empty_dir, mock.Mock())
 
 
-def test_run_without_errors(mocker, temp_empty_dir):
-    with mocker.patch('subprocess.check_call'):
+def test_run_without_errors(temp_empty_dir):
+    with mock.patch('subprocess.check_call'):
         vr.VCSRepo._set_command = _test_set_command
-        repo = vr.VCSRepo(temp_empty_dir, mocker.Mock())
+        repo = vr.VCSRepo(temp_empty_dir, mock.Mock())
 
-    with mocker.patch('subprocess.Popen') as mock_popen:
-        mock_popen_obj = mocker.Mock()
+    with mock.patch('subprocess.Popen') as mock_popen:
+        mock_popen_obj = mock.Mock()
         mock_popen.return_value = mock_popen_obj
         mock_popen_obj.communicate.return_value = \
             ("stdout".encode("utf8"), "stderr".encode("utf8"))
@@ -37,13 +42,13 @@ def test_run_without_errors(mocker, temp_empty_dir):
         assert repo._run([repo.command, "--help"]) == "stdout"
 
 
-def test_run_with_errors(mocker, temp_empty_dir):
-    with mocker.patch('subprocess.check_call'):
+def test_run_with_errors(temp_empty_dir):
+    with mock.patch('subprocess.check_call'):
         vr.VCSRepo._set_command = _test_set_command
-        repo = vr.VCSRepo(temp_empty_dir, mocker.Mock())
+        repo = vr.VCSRepo(temp_empty_dir, mock.Mock())
 
-    with mocker.patch('subprocess.Popen') as mock_popen:
-        mock_popen_obj = mocker.Mock()
+    with mock.patch('subprocess.Popen') as mock_popen:
+        mock_popen_obj = mock.Mock()
         mock_popen.return_value = mock_popen_obj
         mock_popen_obj.communicate.return_value = \
             ("stdout".encode("utf8"), "stderr".encode("utf8"))
@@ -54,7 +59,7 @@ def test_run_with_errors(mocker, temp_empty_dir):
 
         assert str(exc.value) == \
             "An error occurred executing '{} --help':".format(repo.command) + \
-            "stderr\nProcess output was: stdout"
+            " stderr\nProcess output was: stdout"
 
 
 def test_initialize_repo_with_global_configuration_object(
