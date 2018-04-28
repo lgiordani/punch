719a3a46016b47795b1e4bdb3a1717cfafb4a292
Leonardo Giordani <giordani.leonardo@gmail.com>
2018-04-28 22:21:45 +0100

    Fixed wrong multiline string in the default config file

--- punch/cli.py
+++ punch/cli.py
@@ -45,8 +45,8 @@ VERSION = ['major', 'minor', 'patch']
 VCS = {
     'name': 'git',
     'commit_message': (
-        "Version updated from {{ current_version }}",
-        " to {{ new_version }}"),
+        "Version updated from {{ current_version }}"
+        " to {{ new_version }}")
 }
 """
 
